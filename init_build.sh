00ea4471d05d02e62f7f819de33b6e9685c454cf
izenderi <ziliang1121@gmail.com>
2025-04-07 23:36:27 -0700

    gix init_build

--- init_build.sh
+++ init_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf build/ && mkdir build 
-# cp data.zip ./build && cd build # when you obtained the data.zip file, you can save it and do not need to download it again
+rm -rf build/ && mkdir build && cd build
+# rm -rf build/ && mkdir build && cp data.zip ./build && cd build # when you obtained the data.zip file, you can save it and do not need to download it again
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DYAML_FILE=profiles/native_gl.yaml
 cmake --build . -j$(nproc)
 cmake --build . -t docs 
